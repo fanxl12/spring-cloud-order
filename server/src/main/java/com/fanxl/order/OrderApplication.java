a0eaeed6ca13f6f04eaf2250a92c8a66f3999961
fanxl12 <744822514@qq.com>
2018-12-20 21:30:34 +0800

    添加Hystrix监控;

--- server/src/main/java/com/fanxl/order/OrderApplication.java
+++ server/src/main/java/com/fanxl/order/OrderApplication.java
@@ -2,6 +2,7 @@ package com.fanxl.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 //@EnableCircuitBreaker
 @SpringCloudApplication
 @ComponentScan(basePackages = "com.fanxl")
+@EnableHystrixDashboard
 public class OrderApplication {
 
     public static void main(String[] args) {
